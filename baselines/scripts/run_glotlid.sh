632352f8c31a42f71a59f565221b045a1ea18224
Jindra Helcl <jindra.helcl@gmail.com>
2025-08-27 00:42:50 +0300

    do not redirect stderr to log, let slurm do it

--- baselines/scripts/run_glotlid.sh
+++ baselines/scripts/run_glotlid.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p results
 
 echo "running on dev"
-python3 scripts/glotlid.py dev > results/flores_plus_dev_glotlid_predictions.jsonl 2>logs/glotlid_dev.log
+python3 scripts/glotlid.py dev > results/flores_plus_dev_glotlid_predictions.jsonl
 
 echo "running on devtest"
-python3 scripts/glotlid.py devtest > results/flores_plus_devtest_glotlid_predictions.jsonl 2>logs/glotlid_devtest.log
+python3 scripts/glotlid.py devtest > results/flores_plus_devtest_glotlid_predictions.jsonl
