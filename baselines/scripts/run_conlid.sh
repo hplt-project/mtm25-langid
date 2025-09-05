a77145f3a51f5e96d90e04e283c783ae20db124a
Jindra Helcl <helcl@ufal.mff.cuni.cz>
2025-09-05 16:14:03 +0200

    fix path to conlid model

--- baselines/scripts/run_conlid.sh
+++ baselines/scripts/run_conlid.sh
@@ -8,12 +8,12 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)/3rd_party/ConLID"
 mkdir -p results
 
 echo "Running conlid on dev"
-python3 scripts/conlid_predictions.py --dataset flores --model conlid --model-dir models/conlid --split dev > results/flores_plus_dev_conlid_predictions.jsonl
+python3 scripts/conlid_predictions.py --dataset flores --model conlid --model-dir models/conlid-model --split dev > results/flores_plus_dev_conlid_predictions.jsonl
 
 echo "Running conlid on devtest"
-python3 scripts/conlid_predictions.py --dataset flores --model conlid --model-dir models/conlid --split devtest > results/flores_plus_devtest_conlid_predictions.jsonl
+python3 scripts/conlid_predictions.py --dataset flores --model conlid --model-dir models/conlid-model --split devtest > results/flores_plus_devtest_conlid_predictions.jsonl
 
 echo "Running conlid on udhr"
-python3 scripts/conlid_predictions.py --dataset udhr --model conlid --model-dir models/conlid > results/udhr_conlid_predictions.jsonl
+python3 scripts/conlid_predictions.py --dataset udhr --model conlid --model-dir models/conlid-model > results/udhr_conlid_predictions.jsonl
 
 echo "Completed running conlid on both splits"
